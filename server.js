3f097287c766f194cf13143fec7eebebebffa0c5
Raven Taylor <raven61@live..unc.edu>
2022-04-30 17:21:13 -0400

    testing

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-// require express.js
+//import { get } from "express/lib/response";
 
-const express = require('express')
-const app = express()
+//import { coinFlip, coinFlips, flipACoin } from "./coin.mjs";
+import { coinFlip, coinFlips, flipACoin } from "./coin.mjs";
+import express from "express";
+import minimist from "minimist";
+const app = express();
+
+const args = minimist(process.argv.slice(2));
+
+const HTTP_PORT = args["port"] || 5000;
 
 //start an app server
 const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
+  console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
 //default respnse for any other request
-app.use(function(req, res) {
-    res.status(404).send('404 NOT FOUND')
-})
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
- });
+app.get("/app/", (req, res) => {
+  res.status(200).json("200 OK");
+});
 
- app.get('/app/flips/:number', (req, res) => {
-	//Some
-	//expressions
-	//go
-	//here
-    // you can make use of the parameter number by req.params.number
+app.get("/app/flip", (req, res, next) => {
+  res.status(200).json({ flip: coinFlip() });
+});
+
+app.get("/app/flips/:number", (req, res, next) => {
+  res.status(200).json(coinFlips(req.params.number));
+});
+
+app.get("/app/flip/call/heads", (req, res, next) => {
+  res.status(200).json(flipACoin("heads"));
+});
+
+app.get("/app/flip/call/tails", (req, res, next) => {
+  res.status(200).json(flipACoin("tails"));
+});
+
+app.use(function (req, res) {
+  res.status(404).send("404 NOT FOUND");
 });
